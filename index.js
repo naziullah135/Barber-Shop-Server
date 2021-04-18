847dbc30bc463a0df0bb788a75cb19a921234efa
Naziullah Shawn <naziullahshawn135@gmail.com>
2021-04-18 11:06:48 +0600

    finish

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const port = process.env.PORT || 5000;
 
-const app = express();
-
-app.use(bodyParser.json());
 app.use(cors());
-
-const port = 5000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello from db it's working working");
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbnje.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
-    useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 client.connect((err) => {
-  console.log('connection error',err);
-  const serviceCollection = client.db("BarberShop").collection("services");
-  
+  console.log(err);
+  const serviceCollection = client.db("BarberShop").collection("services")
+  const reviewCollection = client.db("BarberShop").collection("reviews")
+
   app.post("/addService", (req, res) => {
     const service = req.body;
-    serviceCollection.insertOne(service)
-    .then((result) => {
+    console.log("add service", service);
+    serviceCollection.insertOne(service).then((result) => {
+      console.log("inserted count", result.insertedCount);
       res.send(result.insertedCount > 0);
     });
-    console.log(service);
   });
 
   app.get("/services", (req, res) => {
-    serviceCollection.find()
-    .toArray((err, documents) => {
-      res.send(documents);
+    serviceCollection.find().toArray((err, services) => {
+      res.send(services);
     });
   });
+
+  app.post("/addReview", (req, res) => {
+    const review = req.body;
+    console.log("add review", review);
+    reviewCollection.insertOne(review).then((result) => {
+      console.log("inserted count", result.insertedCount);
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/reviews", (req, res) => {
+    reviewCollection.find().toArray((err, review) => {
+      res.send(review);
+    });
+  });
+  
 });
 
-app.listen(process.env.PORT || port);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
